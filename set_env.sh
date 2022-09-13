b801e70587ccd704f0d389e0f2391f45a40656ca
Le Tan Hai <tanhai@MacBook-Pro.local>
2022-09-13 15:38:42 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=project3-database.cwj4sfbiomfk.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=prj3-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=tunva
+export JWT_SECRET=testing
+export URL=http://localhost:8100

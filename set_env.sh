56632ecc86501d92ab2ec2cac8a1e09f08f1676a
Le Tan Hai <tanhai@MacBook-Pro.local>
2022-09-17 11:41:33 +0700

    update environment config

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=project3-db.cwj4sfbiomfk.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=proj3-db.cwj4sfbiomfk.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=proj3-db
 export AWS_BUCKET=uda-proj3-bucket
 export AWS_REGION=us-east-1
